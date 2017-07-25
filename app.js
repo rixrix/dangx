d575d60577f8f2d9a4411aa29618b15d785eb5f6
Richard Sentino <richard@mindginative.com>
2017-07-26 00:16:51 +1200

    feat: add sample api routes

--- app.js
+++ app.js
@@ -4,14 +4,21 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-const PUBLIC_DIR = path.resolve(__dirname, './public');
+const routes = require('./routes/index');
+
+app.use('/user', routes.user(express));
+app.use('/dashboard', routes.dashboard(express));
 
 app.get('/', (req, res) => {
-    res.sendFile('index.html', {
-        root: PUBLIC_DIR
+    res.status(200).json({
+        message: 'hello, world!'
     });
 });
 
-app.listen(9001, () => {
-    console.log('NodeJS app listening on port 9001!');
+app.get('*', (req, res) => {
+    res.redirect('/');
+});
+
+app.listen(8081, () => {
+    console.log('NodeJS app listening on port 8081');
 });
